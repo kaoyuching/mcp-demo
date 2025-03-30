325e96339b09c2d7bd9e871c21c2745fef95060c
Doris Kao <doris830426@gmail.com>
2025-03-30 09:57:12 +0800

    ADD: get stock symbol mapping and prompt template for realtime price searching

--- tools_server.py
+++ tools_server.py
@@ -1,9 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
 import yfinance as yf
 from mcp.server.fastmcp import FastMCP
 
 
 # initialize MCP server
 mcp = FastMCP("stock", dependencies=["yfinance"])
+
+
+# unit functions
+def get_tw_stock_symbols():
+    urls = ["https://isin.twse.com.tw/isin/C_public.jsp?strMode=2",  # listed
+        "https://isin.twse.com.tw/isin/C_public.jsp?strMode=3",  # bond
+        "https://isin.twse.com.tw/isin/C_public.jsp?strMode=4"  # otc
+    ]
+
+    symbol_mapping = {}
+    for url in urls:
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        for x in soup.find_all('tr')[2:]:
+          symbol = x.find('td').text.split('\u3000')
+          if len(symbol) == 2:
+            symbol_mapping[symbol[1]] = symbol[0] + ".TW"
+    return symbol_mapping
+
+symbol_mapping = get_tw_stock_symbols()
 
 
 # tools
@@ -31,6 +53,25 @@ def get_stock_previous_close(ticker_symbol: str):
     ticker = yf.Ticker(ticker_symbol)
     previous_close = ticker.info['regularMarketPreviousClose']
     return previous_close
+
+
+@mcp.tool()
+def get_stock_symbol(name: str):
+    """
+    Search stock symbol by company name.
+    """
+    return symbol_mapping[name]
+    
+
+# prompt template
+def ask_realtime_stock_price(company: str):
+    """
+    Ask realtime stock price.
+
+    Args:
+        company: company name
+    """
+    return f"請問{company}現在的股價多少?"
 
 
 if __name__ == "__main__":
